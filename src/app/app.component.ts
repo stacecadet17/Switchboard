de7cb8fcbf4c2ed6748c1ed8696f198353c7e62f
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-10-17 16:10:36 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  switches = [true, true, true, true, true, true, true, true, true, true];
+  flipSwitch(idx) {
+  this.switches[idx] = !this.switches[idx];
+  }
 }
